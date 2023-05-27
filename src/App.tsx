64fb67a6a6d9eae8c08f0a866c1adcbc1d697967
Daniel Stehura <stehura.daniel@gmail.com>
2023-05-27 04:56:32 +0200

    React init

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,24 @@
-import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-
-import List from "./components/List";
-import Lists from "./components/Lists";
-import AddList from "./components/AddList";
-import AddItem from "./components/AddItem";
-
-const App: React.FC = () => {
+function App() {
   return (
-    <div>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/lists" className="navbar-brand">
-          SZM
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/lists"} className="nav-link">
-              Lists
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/add"} className="nav-link">
-              Add new
-            </Link>
-          </li>
-        </div>
-      </nav>
-
-      <div className="container mt-3">
-        <Routes>
-          <Route path="/" element={<Lists/>} />
-          <Route path="/lists" element={<Lists/>} />
-          <Route path="/add" element={<AddList/>} />
-          <Route path="/lists/:id" element={<List/>} />
-          <Route path="/lists/:id/add-item" element={<AddItem/>} />
-        </Routes>
-      </div>
+      </header>
     </div>
   );
 }
--- src/types/enums.ts
+++
@@ -1,8 +0,0 @@
-export enum MESSAGE_TYPE {
-	ERROR = 'ERROR',
-	WARNING = 'WARNING',
-	SUCCESS = 'SUCCESS',
-	INFO = 'INFO'
-}
-
-export const MESSAGE_TYPES = Object.values(MESSAGE_TYPE)
